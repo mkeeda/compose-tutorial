9f092c410e06795328ffb794718d1add7582b894
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-11-01 15:00:46 +0900

    Layouts

--- app/src/main/java/com/mkeeda/composetutorial/MainActivity.kt
+++ app/src/main/java/com/mkeeda/composetutorial/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.layout.LayoutSize
+import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 
@@ -13,21 +17,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                NewsStory()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        crossAxisSize = LayoutSize.Expand,
+        modifier = Spacing(16.dp)
+    ) {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        Greeting("aaaaaaaaaaaaaAndroid")
+        NewsStory()
     }
 }
