2418448f4f2653b0f25f0118182af62e464265b3
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-11-01 16:29:55 +0900

    Add a image

--- app/src/main/java/com/mkeeda/composetutorial/MainActivity.kt
+++ app/src/main/java/com/mkeeda/composetutorial/MainActivity.kt
@@ -3,13 +3,18 @@ package com.mkeeda.composetutorial
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.foundation.DrawImage
 import androidx.ui.layout.Column
+import androidx.ui.layout.Container
+import androidx.ui.layout.HeightSpacer
 import androidx.ui.layout.LayoutSize
 import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -25,10 +30,18 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
+    val image = +imageResource(R.drawable.header)
+
     Column(
         crossAxisSize = LayoutSize.Expand,
         modifier = Spacing(16.dp)
     ) {
+        Container(expanded = true, height = 180.dp) {
+            DrawImage(image = image)
+        }
+
+        HeightSpacer(height = 16.dp)
+
         Text(text = "A day in Shark Fin Cove")
         Text(text = "Davenport, California")
         Text(text = "December 2018")
